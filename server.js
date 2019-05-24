faffd74b75ca88b77aa1067e6e5c07a5c7ae2765
Skablov <danilaskablov@list.ru>
2019-05-24 03:32:24 +0300

    add /register for register new users

--- server.js
+++ server.js
@@ -11,19 +11,40 @@ app.use(BodyParser.urlencoded({ extended: true }));
 
 Mongoose.connect(config.URL, { useNewUrlParser: true });
 const modelUser = Model.user;
-
-app.post('/person', async(req, res) =>{
+app.post('/signin', async(req, res)=>{
+  try{
+    if(!req.body.login || !req.body.password){
+      return res.json({ error: "Не введен пароль или логин!" });
+    }
+    else{
+        var check = await modelUser.findOne({login: req.body.login}).exec();
+        if(check.length != 0 && check.password == req.body.password){
+          return res.send(`Вы вошли как ${check.login}`);
+        } else{
+          return res.send("Неправильный логин или пароль!");
+        }
+    };
+  }catch(error){
+    res.status(500).send(error);
+  }
+})
+app.post('/register', async(req, res) =>{
   try{
     if(!req.body.login || !req.body.password){
       return res.json({error: "Не введен пароль или логин"});
     }
     else{
-      var user = new modelUser(req.body);
-      var result = await user.save();
-      res.send(result);
+      var check = await modelUser.findOne({login: req.body.login}).exec();
+      if(check.length == 0){
+        var user = new modelUser(req.body);
+        var result = await user.save();
+        return res.send(`Вы успешно зарегистрировались как ${result.login}, теперь вы можете авторизоваться!`);
+      }else{
+        return res.json({ error: "Данный логин уже используется в системе :(" });
+      }
     };
   }catch(error){
-    res.status(500).send(error);
+    return res.status(500).send(error);
   };
 })
 
